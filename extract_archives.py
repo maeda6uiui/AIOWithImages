24c2b8d66067d2b2cb1d0779da5fa23ca43b173b
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2020-06-19 16:27:30 +0900

    Initial commit

---
+++ extract_archives.py
@@ -0,0 +1,20 @@
+import logging
+import zipfile
+import os
+
+ARCHIVE_DIR="./WikipediaImages/Archives/"
+IMAGE_DIR="./WikipediaImages/Images/"
+
+logger=logging.getLogger("awi")
+
+if __name__=="__main__":
+    logger.info("ZIPファイルの解凍を開始します。")
+    for i in range(0,114):
+        logging.info("{}".format(i))
+        
+        os.mkdir(IMAGE_DIR+str(i))
+
+        with zipfile.ZipFile(ARCHIVE_DIR+str(i)+".zip") as r:
+            r.extractall(IMAGE_DIR+str(i))
+
+    logger.info("ZIPファイルの解凍が終了しました。")
