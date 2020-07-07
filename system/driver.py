3aab252442d4c1d67f647811cb8202e8dde8c9a8
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2020-07-07 21:59:09 +0900

    コードを追加

--- system/driver.py
+++ system/driver.py
@@ -1,63 +1,68 @@
+import os
 import subprocess
 
-class TrainAndTestDriver(object):
+class TrainAndTestScriptGenerator(object):
     def __init__(
         self,system_name="jaqket_baseline",
         data_dir="../Data/",
         model_name_or_path="cl-tohoku/bert-base-japanese-whole-word-masking",
-        task_name="jaqket",
         output_dir="./working_dir/output_dir/"):
+        self.system_name=system_name
+
         self.command=(
-            "python3 ../system/{system_name}.py\n"
-            "--data_dir {data_dir}\n"
-            "--model_name_or_path {model_name_or_path}\n"
-            "--task_name {task_name}\n"
-            "--entities_fname candidate_entities.json.gz\n"
-            "--train_fname train_questions.json\n"
-            "--dev_fname dev1_questions.json\n"
-            "--test_fname dev2_questions.json\n"
-            "--output_dir {output_dir}\n"
-            "--train_num_options 4\n"
-            "--do_train\n"
-            "--do_eval\n"
-            "--do_test\n"
-            "--per_gpu_train_batch_size 1\n"
-            "--gradient_accumulation_steps 8\n"
-            "--num_train_epochs 5\n"
-            "--logging_steps 10\n"
-            "--save_steps 1000"
+            "python3 ../system/{system_name}.py \\\n"
+            "\t--data_dir {data_dir} \\\n"
+            "\t--model_name_or_path {model_name_or_path} \\\n"
+            "\t--task_name {system_name} \\\n"
+            "\t--entities_fname candidate_entities.json.gz \\\n"
+            "\t--train_fname train_questions.json \\\n"
+            "\t--dev_fname dev1_questions.json \\\n"
+            "\t--test_fname dev2_questions.json \\\n"
+            "\t--output_dir {output_dir} \\\n"
+            "\t--train_num_options 4 \\\n"
+            "\t--do_train \\\n"
+            "\t--do_eval \\\n"
+            "\t--do_test \\\n"
+            "\t--per_gpu_train_batch_size 1 \\\n"
+            "\t--gradient_accumulation_steps 8 \\\n"
+            "\t--num_train_epochs 5 \\\n"
+            "\t--logging_steps 10 \\\n"
+            "\t--save_steps 1000\n"
         ).format(
             system_name=system_name,
             data_dir=data_dir,
             model_name_or_path=model_name_or_path,
-            task_name=task_name,
             output_dir=output_dir
         )
-        self.command=self.command.split("\n")
 
         self.command2=(
-            "python3 ../system/{system_name}.py\n"
-            "--data_dir {data_dir}\n"
-            "--dev_fname dev1_questions.json\n"
-            "--test_fname dev2_questions.json\n"
-            "--task_name {task_name}\n"
-            "--entities_fname candidate_entities.json.gz\n"
-            "--model_name_or_path {output_dir}\n"
-            "--eval_num_options 20\n"
-            "--per_gpu_eval_batch_size 4\n"
-            "--do_test\n"
-            "--do_eval"
+            "python3 ../system/{system_name}.py \\\n"
+            "\t--data_dir {data_dir} \\\n"
+            "\t--dev_fname dev1_questions.json \\\n"
+            "\t--test_fname dev2_questions.json \\\n"
+            "\t--task_name {system_name} \\\n"
+            "\t--entities_fname candidate_entities.json.gz \\\n"
+            "\t--model_name_or_path {output_dir} \\\n"
+            "\t--eval_num_options 20 \\\n"
+            "\t--per_gpu_eval_batch_size 4 \\\n"
+            "\t--do_test \\\n"
+            "\t--do_eval \\\n"
         ).format(
             system_name=system_name,
             data_dir=data_dir,
-            task_name=task_name,
             output_dir=output_dir
         )
-        self.command2=self.command2.split("\n")
 
-    def run(self):
-        res=subprocess.check_output(self.command)
-        print(res)
+    def save_script(self,save_dir):
+        os.makedirs(save_dir,exist_ok=True)
 
-        res2=subprocess.check_output(self.command2)
-        print(res2)
+        script_filename="run_{}_all.sh".format(self.system_name)
+        with open(save_dir+script_filename,"w",encoding="utf-8") as w:
+            w.write(self.command)
+            w.write("\n")
+            w.write(self.command2)
+
+if __name__=="__main__":
+    driver=TrainAndTestScriptGenerator()
+    driver.save_script("./working_dir/")
+    
